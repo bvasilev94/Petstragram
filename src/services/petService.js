90735f5036df754320a8d11e5cc7dd6fb683c53d
biservasilev <biservasilev94@gmail.com>
2023-06-27 00:34:18 +0300

    Update petService.js

--- src/services/petService.js
+++ src/services/petService.js
@@ -3,6 +3,7 @@ const Pet = require("../models/Pet.js");
 exports.create = (petData) => Pet.create(petData);
 exports.getAllPets = () => Pet.find().populate("owner").lean();
 exports.getOnePet = (petId) => Pet.findById(petId).populate("owner").lean();
+// exports.getOnePet = (petId) => Pet.findOne({"_id": petId}).populate("owner").lean();
 exports.addComment = async (petId, username, comment) => {
   const pet = await Pet.findById(petId);
 
